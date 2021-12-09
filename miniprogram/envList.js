b89566eb591d7c5927ce3aac59de9a26dd10db89
李离狸 <luoqaq@163.com>
2021-12-09 23:36:45 +0800

    创建项目

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-4g8nibyi77844097","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"G:\小程序\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-4g8nibyi77844097 --n quickstartFunctions --r --project "G:\小程序\First" --report_first --report
